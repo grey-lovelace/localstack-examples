c0689e75192ed41321b3a153ad49f854a1826f40
Grey Lovelace <austin.grey.lovelace@gmail.com>
2022-10-04 15:10:03 -0500

    update localsteack image and add s3

--- localstack/setup.sh
+++ localstack/setup.sh
@@ -1,30 +1,45 @@
 #!/bin/sh -x
+
+alias aws="aws --endpoint-url=$URL"
+export BUCKET1_NAME=bucket1
+export BUCKET2_NAME=bucket2
 export PRIMARY_SQS=queue
 export SECONDARY_SQS=queue-secondary
 export DLQ_SQS=queue-dead-letter
 
-echo "########### Create Dead Letter Queue and get ARN ###########"
-aws sqs create-queue --endpoint-url=$URL --queue-name $DLQ_SQS
+while ! aws iam get-user
+do
+    echo "########### Waiting for localstack to be available ###########"
+done
 
-DLQ_SQS_ARN=$(aws sqs get-queue-attributes --endpoint-url=$URL --attribute-name QueueArn --queue-url=$URL_LOCAL/000000000000/"$DLQ_SQS"\
+echo "########### Create Buckets ###########"
+aws s3api create-bucket --bucket $BUCKET1_NAME
+aws s3api create-bucket --bucket $BUCKET2_NAME
+
+echo "########### Create Dead Letter Queue and get ARN ###########"
+aws sqs create-queue --queue-name $DLQ_SQS
+
+DLQ_SQS_ARN=$(aws sqs get-queue-attributes --attribute-name QueueArn --queue-url=$URL_LOCAL/000000000000/"$DLQ_SQS"\
   |  sed 's/"QueueArn"/\n"QueueArn"/g' | grep '"QueueArn"' | awk -F '"QueueArn":' '{print $2}' | tr -d '"' | xargs)
 
 echo "########### Create Seconday Queue and get ARN ###########"
-aws sqs create-queue --endpoint-url=$URL --queue-name $SECONDARY_SQS \
+aws sqs create-queue --queue-name $SECONDARY_SQS \
   --attributes '{
       "RedrivePolicy": "{\"deadLetterTargetArn\":\"'"$DLQ_SQS_ARN"'\",\"maxReceiveCount\":\"10\"}",
       "VisibilityTimeout": "1200"
     }'
 
-SECONADRY_SQS_ARN=$(aws sqs get-queue-attributes --endpoint-url=$URL --attribute-name QueueArn --queue-url=$URL_LOCAL/000000000000/"$SECONDARY_SQS"\
+SECONADRY_SQS_ARN=$(aws sqs get-queue-attributes --attribute-name QueueArn --queue-url=$URL_LOCAL/000000000000/"$SECONDARY_SQS"\
   |  sed 's/"QueueArn"/\n"QueueArn"/g' | grep '"QueueArn"' | awk -F '"QueueArn":' '{print $2}' | tr -d '"' | xargs)
 
 echo "########### Create Primary Queue ###########"
-aws sqs create-queue --endpoint-url=$URL --queue-name $PRIMARY_SQS \
+aws sqs create-queue --queue-name $PRIMARY_SQS \
   --attributes '{
       "RedrivePolicy": "{\"deadLetterTargetArn\":\"'"$SECONADRY_SQS_ARN"'\",\"maxReceiveCount\":\"1\"}",
       "VisibilityTimeout": "1200"
     }'
 
-echo "########### Listing queues ###########"
-aws sqs list-queues --endpoint-url=$URL
+echo "########### Listing Buckets ###########"
+aws s3 ls
+echo "########### Listing Queues ###########"
+aws sqs list-queues 
